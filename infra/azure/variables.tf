e6fffbc9235f29fcb649eda26ede87d83fb6e11f
Abbah Divine <abbahattamah@gmail.com>
2025-08-31 22:23:28 +0100

    Update variables.tf

--- infra/azure/variables.tf
+++ infra/azure/variables.tf
@@ -7,3 +7,25 @@ variable "admin_username" {
   description = "Admin username for the VM"
   default     = "azureuser"
 }
+
+variable "dockerhub_username" {
+  description = "Username for Docker Hub authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "dockerhub_token" {
+  description = "Access token for Docker Hub authentication"
+  type        = string
+  sensitive   = true
+}
+
+variable "docker_image" {
+  description = "Name of the Docker image to deploy"
+  type        = string
+}
+
+variable "image_tag" {
+  description = "Tag of the Docker image to deploy"
+  type        = string
+}
